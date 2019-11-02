123bbb830557b9b1406c7242d8cd714381dd10c6
pvannyamelia <50089393+pvannyamelia@users.noreply.github.com>
2019-11-02 22:33:28 +0700

    Check-out done

--- project kelompok/js/check-out.js
+++ project kelompok/js/check-out.js
@@ -1,8 +1,16 @@
-function nyala(cb){
+function nyala(){
+    var cb = document.getElementById("send-details");
     var senddetails = document.getElementById("cb-details");
 if(cb.checked == true){
 senddetails.disabled = false;
 }else{
     senddetails.disabled = true;
 }
+}
+
+function cekemail(){
+    var email = document.getElementById("cb-details").value;
+    if(email == "email"){
+        document.getElementById("email").required = true;
+    }
 }
