72e45bda25322c797805e5f16b8847279f58251d
kzyc26 <50088876+kzyc26@users.noreply.github.com>
2019-11-05 20:10:16 +0700

    function
    coba tmbh function

---
+++ project kelompok/js/accountsetting.js
@@ -0,0 +1,12 @@
+document.querySelector('.subcatprofile').style.display = 'none';
+document.querySelector('.subcatnotifications').style.display = 'none';
+document.querySelector('.subcatorderhistory').style.display = 'none';
+document.querySelector('.subcatvoucher').style.display = 'none';
+document.querySelector('.subcatpolicy').style.display = 'none';
+
+
+function changecontent(accountcontent){
+    document.querySelector('.subcatprofile').style.display = 'block';
+};
+
+
