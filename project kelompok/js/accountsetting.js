7c9c30c2c0007c9da1052869d46a0496b75dc33b
kzyc26 <50088876+kzyc26@users.noreply.github.com>
2019-11-06 22:08:04 +0700

    updates
    tmbh function edit footer

--- project kelompok/js/accountsetting.js
+++ project kelompok/js/accountsetting.js
@@ -5,8 +5,43 @@ document.querySelector('.subcatvoucher').style.display = 'none';
 document.querySelector('.subcatpolicy').style.display = 'none';
 
 
-function changecontent(accountcontent){
+function changeaccount(){
     document.querySelector('.subcatprofile').style.display = 'block';
+    document.querySelector('.subcatnotifications').style.display = 'none';
+    document.querySelector('.subcatorderhistory').style.display = 'none';
+    document.querySelector('.subcatvoucher').style.display = 'none';
+    document.querySelector('.subcatpolicy').style.display = 'none';
 };
+function changetovoucher(){
+    document.querySelector('.subcatprofile').style.display = 'none';
+    document.querySelector('.subcatnotifications').style.display = 'none';
+    document.querySelector('.subcatorderhistory').style.display = 'none';
+    document.querySelector('.subcatvoucher').style.display = 'block';
+    document.querySelector('.subcatpolicy').style.display = 'none';
+}
+function shownotif(){
+    document.querySelector('.subcatprofile').style.display = 'none';
+    document.querySelector('.subcatnotifications').style.display = 'block';
+    document.querySelector('.subcatorderhistory').style.display = 'none';
+    document.querySelector('.subcatvoucher').style.display = 'none';
+    document.querySelector('.subcatpolicy').style.display = 'none';
+    document.querySelector(".subcatwishlist").style.display = 'none';
+}
+function showwishlist(){
+    document.querySelector('.subcatprofile').style.display = 'none';
+    document.querySelector('.subcatnotifications').style.display = 'none';
+    document.querySelector('.subcatorderhistory').style.display = 'none';
+    document.querySelector('.subcatvoucher').style.display = 'none';
+    document.querySelector('.subcatpolicy').style.display = 'none';
+    document.querySelector(".subcatwishlist").style.display = 'block';
+}
 
+function showhistory(){
+    document.querySelector('.subcatprofile').style.display = 'none';
+    document.querySelector('.subcatnotifications').style.display = 'none';
+    document.querySelector('.subcatorderhistory').style.display = 'block';
+    document.querySelector('.subcatvoucher').style.display = 'none';
+    document.querySelector('.subcatpolicy').style.display = 'none';
+    document.querySelector(".subcatwishlist").style.display = 'none';
+}
 
