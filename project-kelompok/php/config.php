1550a0cedae3d61ce65477fdd0d73e237f3ab5a6
pvannyamelia <50089393+pvannyamelia@users.noreply.github.com>
2019-11-12 13:29:18 +0700

    config and db .php added

---
+++ project-kelompok/php/config.php
@@ -0,0 +1,7 @@
+<?php
+    $base_url = "http://localhost/webdev/project-kelompok/project-kelompok/php/";
+    $servername = "localhost";
+    $username 	= "root";
+    $password 	= "";
+    $dbname 	= "toko_pojok_70";
+?>
