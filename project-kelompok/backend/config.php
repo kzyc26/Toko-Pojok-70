06bdb7f6c432572e9a247d25b46a62d13bcbd89e
pvannyamelia <50089393+pvannyamelia@users.noreply.github.com>
2019-12-05 11:55:52 +0700

    aa

---
+++ project-kelompok/backend/config.php
@@ -0,0 +1,9 @@
+<?php
+// $base_url = "http://localhost/webdev/project-kelompok/project-kelompok/ajax_coba/"
+    // $base_url = "http://jmswijaya.com/phpmyadmin/"; //database
+    $base_url = "localhost/webdev/project-kelompok/project-kelompok/";
+    $servername = 'jmswijaya.com';
+    $username 	= 'isb18';
+    $password 	= 'Isb_2018';
+    $dbname 	= 'store18_2';
+?>
---
+++ project-kelompok/backend/footer.php
@@ -0,0 +1,8 @@
+<link href="../css/backend.css" rel="stylesheet">
+<script src="../assets/bootstrap-3.4.1-dist/js/bootstrap.js"></script>
+<link href="../assets/bootstrap-3.4.1-dist/css/bootstrap.css" rel="stylesheet">
+<script src="../js/backend.js"></script>
+
+</body>
+
+</html>
