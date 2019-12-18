22dafdef89e2e8f0fe1a6c88e06f36e9423115dd
kzyc26 <50088876+kzyc26@users.noreply.github.com>
2019-12-18 15:25:19 +0700

    menu jadi accordion

--- database/header.php
+++ database/header.php
@@ -15,47 +15,100 @@
         <div class="row">
             <div class="col-md-3 report">
                 <h2>Report</h2>
-                <a href="user.php">Login Details Pengguna</a>
-                <br>
-                <a href="accumulated.php">Customer Review Accumulated per Product</a>
-                <br>
-                <a href="customer_review.php">Individual Customer Review</a>
-                <br>
-                <a href="customer_subtotal.php">Customer Subtotal</a>
-                <br>
+                <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                    <div class="panel panel-default">
+                        <div class="panel-heading" role="tab" id="headingOne">
+                            <h4 class="panel-title">
+                                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"
+                                    aria-expanded="true" aria-controls="collapseOne">
+                                    User
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel"
+                            aria-labelledby="headingOne">
+                            <div class="panel-body">
+                                <a href="user.php">Login Details Pengguna</a>
+                                <br>
+                                <a href="alamat.php">Alamat User</a>
+                                <br>
+                                <a href="biodata.php">Biodata</a>
+                                <br>
+                                <a href="wishlist.php">Wishlist</a>
+                                <br>
+                                <a href="voucher.php">Voucher Customer</a>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading" role="tab" id="headingTwo">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+                                    href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    Products
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel"
+                            aria-labelledby="headingTwo">
+                            <div class="panel-body">
+                                <a href="accumulated.php">Customer Review Accumulated per Product</a>
+                                <br>
+                                <a href="customer_review.php">Individual Customer Review</a>
+                                <br>
+                                <a href="items_per_category.php">Items Per Category</a>
+                                <br>
+                                <a href="bestseller.php">Best Seller Products</a>
+                                <br>
+                                <a href="mostwanted.php">Most Wanted Products</a>
+                                <br>
+                                <a href="discount.php">Product dengan Diskon</a>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading" role="tab" id="headingThree">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+                                    href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    Transaction & Deliveries
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseThree" class="panel-collapse collapse" role="tabpanel"
+                            aria-labelledby="headingThree">
+                            <div class="panel-body">
+                                <a href="invoice.php">Invoice per Hari</a>
+                                <br>
+                                <a href="jumlah_penjualan_perhari.php">Jumlah Penjualan per Hari</a>
+                                <br>
+                                <a href="jumlah_penjualan_perbulan.php">Jumlah Penjualan per Bulan</a>
+                                <br>
+                                <a href="item_details_in_cart.php">Item Details in Cart</a>
+                                <br>
+                                <a href="orderhistory.php">User Order History</a>
+                                <br>
+
+                                <a href="oldpass.php">Frequently Used Ekspedisi</a>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <!-- <a href="customer_subtotal.php">Customer Subtotal</a>
+                <br> -->
                 <!-- <a href="product_details.php">Product Details</a>
                 <br> -->
-                <a href="items_per_category.php">Items Per Category</a>
-                <br>
-                <a href="item_details_in_cart.php">Item Details in Cart</a>
-                <br>
-                <a href="wishlist.php">Wishlist</a>
-                <br>
-                <a href="invoice.php">Invoice per Hari</a>
-                <br>
-                <a href="jumlah_penjualan_perhari.php">Jumlah Penjualan per Hari</a>
-                <br>
-                <a href="jumlah_penjualan_perbulan.php">Jumlah Penjualan per Bulan</a>
-                <br>
-                <a href="voucher.php" >Voucher Customer</a>
-                <br>
-                <a href="orderhistory.php" >User Order History</a>
-                <br>
-                <a href="alamat.php">Alamat User</a>
-                <br>
-                <a href="biodata.php" >Biodata</a>
-                <br>
-                <a href="oldpass.php" >Frequently Used Ekspedisi</a>
-                <br>
+
+
                 <!-- <a href="userdelivdetails.php" >User Delivery Details</a>
                 <br> -->
-                <a href="bestseller.php">Best Seller Products</a>
-                <br>
-                <a href="mostwanted.php">Most Wanted Products</a>
-                <br>
-                <a href="discount.php" >Product dengan Diskon</a>
+
             </div>
-            <div class="seperator">
-            </div>
+
             <div class="col-md-8 result">
                 <h2>Result</h2>
